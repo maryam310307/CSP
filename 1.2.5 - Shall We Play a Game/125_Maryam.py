fdc780c653642e7134e6634ce405d89afeae6bad
maryam310307 <310307@shakopeeschools.org>
2025-10-28 09:17:16 -0500

    Update 125_Maryam.py
    layout

---
+++ 1.2.5 - Shall We Play a Game/125_Maryam.py
@@ -0,0 +1,32 @@
+# Start Screen
+# user will be asked for their name
+# set background for starting screen, title, and explain the game
+# click anywhere to begin
+
+# images
+# import images of all categories: fruits, shapes, colors, etc.
+# around 5 images for each category
+
+# timer
+# timer will show how many seconds the user has left to memorize everything on the screen before 1 disappears
+# timer will decrease every level by a few seconds
+
+# score
+# score will increase by one point every time the user types the missing item in correctly
+# no points will be deducted for getting it wrong but only 1 chance to get it right
+# either get the score or dont, user will be taken to the next level
+
+# leaderboard
+# user's name will be put in the leaderboard depending on their score
+
+
+
+
+
+
+
+
+
+
+wn = trtl.Screen()
+wn.mainloop()
