cdbb5dd81019f6b4211f9a1354d5b7af3cb0802b
maryam310307 <310307@shakopeeschools.org>
2025-10-30 09:45:07 -0500

    1.2.5
    completed starting screen

--- 1.2.5 - Shall We Play a Game/125_Maryam.py
+++ 1.2.5 - Shall We Play a Game/125_Maryam.py
@@ -1,23 +1,53 @@
 import turtle as trtl
 import random as rand
 import time as time
+wn = trtl.Screen()
 
 # Start Screen
-'''wn.bgpic("pink.gif")
-screen_width = 200
-screen_height = 200'''
-trtl.color = "black"
-trtl.write("Welcome to the game!", align="center", font=("Times New Roman", 50))
+# set background for starting screen, title, and explain the game
+wn.bgpic("background.gif")
+screen_width = 500
+screen_height = 50
+trtl.color = "pink"
+trtl.penup()
+'''trtl.goto(-222,200)
+trtl.write("What is missing?", font=("Times New Roman", 50))
+time.sleep(2)
+trtl.clear()
+trtl.goto(-222,100)
+trtl.write("The function of the game is simple.", font=("Times New Roman", 25)) 
+time.sleep(2)
+trtl.clear()
+trtl.goto(-222,70)
+trtl.write("You will be shown 5 images at a time," ,font=("Times New Roman", 25))
+time.sleep(2)
+trtl.clear()
+trtl.goto(-222,40)
+trtl.write("and they will disappear after a set amount of time.", font=("Times New Roman", 25)) 
+time.sleep(2)
+trtl.clear()
+trtl.goto(-222,10)
+trtl.write("Try to guess what is missing!" ,font=("Times New Roman", 25)) 
+time.sleep(2)
+trtl.clear()
+trtl.goto(-130,100)
+trtl.write("Click anywhere to begin", font=("Times New Roman", 25)) 
 trtl.hideturtle()
 
-# user will be asked for their name
-# set background for starting screen, title, and explain the game
-# click anywhere to begin
+# click anywhere to begin and user will be asked for name
+def click_anywhere(x, y):
+    trtl.textinput("What is your name?", "")
+    trtl.clear()
+wn.onscreenclick(click_anywhere)'''
+    
+# import images
+all_fruits = ["apple.gif", "banana.gif", "blueberry.gif", "strawberry.gif", "watermelon.gif"]
+for fruit in all_fruits:
+    wn.addshape(fruit)
 
-
-# images
-# import images of all categories: fruits, shapes, colors, etc.
-# around 5 images for each category
+apple_image = "apple.gif"
+apple_trtl = trtl.Turtle()
+apple_trtl.penup()
 
 # timer
 # timer will show how many seconds the user has left to memorize everything on the screen before 1 disappears
@@ -38,6 +68,4 @@ trtl.hideturtle()
 
 
 
-
-wn = trtl.Screen()
 wn.mainloop()
